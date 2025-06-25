e26638564e9e9d965e7a984b0d88b6915a78170c
Nafiznayan <abdullah191864@gmail.com>
2025-06-25 22:12:42 +0600

    Add additional loading skeletons

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,17 @@
+import { Box } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import delay from "delay";
+const LoadingNewIssuePage = async () => {
+  //await delay(1000); // Simulating a delay for loading state
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
 
-const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+      <Skeleton height="20rem" />
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;
