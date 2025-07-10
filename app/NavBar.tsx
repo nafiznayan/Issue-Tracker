17d20032aab0c8017ceeded7037041d1fe5a2f15
Nafiznayan <abdullah191864@gmail.com>
2025-07-10 13:47:05 +0600

    Add a drop-down menu to show the current user and fixed avatar not loading

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,10 +5,22 @@ import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames"; // package to apply conditional styling
 import { useSession } from "next-auth/react"; // To get the session data
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
+// import * as Avatar from "@radix-ui/react-avatar";
+
 const NavBar = () => {
   const currentPath = usePathname(); // To get the current path we are on
   const { status, data: session } = useSession();
+  console.log("Session status:", status);
+  console.log("Session data:", session);
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -40,7 +52,40 @@ const NavBar = () => {
           </Flex>
           <Box>
             {status === "authenticated" && (
-              <Link href="/api/auth/signout">Log Out</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback={session.user?.name?.[0].toUpperCase() || "U"}
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                    referrerPolicy="no-referrer"
+                  />
+
+                  {/* <Avatar.Root className="w-8 h-8 rounded-full cursor-pointer overflow-hidden bg-zinc-300 flex items-center justify-center">
+                    <Avatar.Image
+                      src={session?.user?.image || ""}
+                      className="w-full h-full object-cover"
+                      alt={session?.user?.name || "User"}
+                    />
+                    <Avatar.Fallback
+                      className="text-sm font-medium text-zinc-700"
+                      delayMs={600}
+                    >
+                      {session?.user?.name?.charAt(0).toUpperCase() || "U"}
+                    </Avatar.Fallback>
+                  </Avatar.Root> */}
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Log Out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             )}
             {status === "unauthenticated" && (
               <Link href="/api/auth/signin">Login</Link>
